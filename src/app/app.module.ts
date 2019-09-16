7202f00e50d99462512e7fb3c5bdd8fbfe95480f
Maryna Petrenko <marinbonibon@gmail.com>
2019-09-16 19:17:10 +0300

    feat: generate components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { BoardComponent } from './board/board.component';
+import { CardListComponent } from './card-list/card-list.component';
+import { CardItemComponent } from './card-item/card-item.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ToolbarComponent,
+    BoardComponent,
+    CardListComponent,
+    CardItemComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
