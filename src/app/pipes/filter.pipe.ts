5fa009aa8ae9927fcc7e3c086dc87c2f2f966719
Maryna Petrenko <marinbonibon@gmail.com>
2019-10-02 21:29:27 +0300

    fix: fix filter pipe

--- src/app/pipes/filter.pipe.ts
+++ src/app/pipes/filter.pipe.ts
@@ -1,13 +1,12 @@
 import {Pipe, PipeTransform} from '@angular/core';
 
 @Pipe({
-  name: 'filter'
+  name: 'filter',
+  pure: false
 })
 export class FilterPipe implements PipeTransform {
 
   transform(value: any[], criterion: string): any[] {
-    console.log('criterion', criterion);
-    console.log('value', value);
     return criterion
       ? value.filter((card: { name: string }) => card.name.startsWith(criterion))
       : value;
